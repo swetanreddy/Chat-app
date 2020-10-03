bdefa05f3394391a8c96902ada110d5460b6b073
swetanreddy <swetanreddys@gmail.com>
2020-10-03 12:05:33 +0530

    my first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var PORT = process.env.PORT || 3000;
+var express = require("express");
+var app = express(); 
+var http = require("http").Server(app);
+app.use(express.static(__dirname + '/main'));
+http.listen(PORT, function() {
+  console.log("server started");
+});
